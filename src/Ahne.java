78c99406817754222297945e8958b9e997df6ffc
thaqime <mehmetthqim@gmail.com>
2025-03-19 11:29:52 +0100

    Anlegen

--- src/Ahne.java
+++ src/Ahne.java
@@ -1,3 +1,36 @@
-public class Ahne {
 
+public class Ahne {
+    private String vorname;
+    private String nachname;
+    private char geschlecht;
+
+    public Ahne(String pVorname,String pNachname, char pGeschlecht){
+
+        vorname= pVorname;
+        nachname= pNachname;
+        geschlecht= pGeschlecht;
+    }
+    public String getVorname(){
+        return vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public char getGeschlecht() {
+        return geschlecht;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public void setGeschlecht(char geschlecht) {
+        this.geschlecht = geschlecht;
+    }
 }
--- src/Ahnenbaum.java
+++ src/Ahnenbaum.java
@@ -1,3 +1,39 @@
+
 import utils.*;
+
+
 public class Ahnenbaum {
+
+    private BinaryTree<Ahne> lisasAhnnenbaum;
+
+    public Ahnenbaum() {
+        lisasAhnnenbaum = new BinaryTree<Ahne>();
+        BinaryTree b1 = new BinaryTree<Ahne>(new Ahne("Jacqueline", "Bouvier", 'w'));
+        BinaryTree b2 = new BinaryTree<Ahne>(new Ahne("Clancy", "Bouvier", 'm'));
+        BinaryTree b3 = new BinaryTree<Ahne>(new Ahne("Marge", "Simpson", 'w'));
+        BinaryTree b4 = new BinaryTree<Ahne>(new Ahne("Mona", "Simpson", 'w'), b1, b2);
+        BinaryTree b5 = new BinaryTree<Ahne>(new Ahne("Abraham", "Simpson", 'm'));
+        BinaryTree b6 = new BinaryTree<Ahne>(new Ahne("Hommer", "Simpson", 'm'), b4, b5);
+        lisasAhnnenbaum = new BinaryTree<Ahne>(new Ahne("Lisa", "Simpson", 'w'), b3, b6);
+
+        public void PreOrder() {// W L R
+            besuche(lisasAhnnenbaum);
+
+        }
+
+        public void besuche(BinaryTree<Ahne> b) {
+
+        }
+
+        public void InOrder () { // L W R
+
+        }
+
+        public void PostOrder () { // L R W
+        }
+
+
+    }
 }
+
+
